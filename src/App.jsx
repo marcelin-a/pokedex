7398a158b0b0cc0a6beddfa0fa5e3d91ee49e4bd
marcelin-a <marcelinalley@yahoo.com>
2024-04-23 22:51:19 +0200

    update : using a state

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,55 @@
+import { useState } from "react";
+
 import PokemonCard from "./components/PokemonCard.jsx";
 
-
-// Insertion tableau pokemonList
 function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  const [pokemonIndex, setpokemonIndex] = useState(0);
 
-  return (
-    <div>
-      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}/>
-    </div>
-    )
+  const handleClickp = () => {
+    setpokemonIndex(pokemonIndex - 1)
+  }
+  
+  const handleClicks = () => {
+    setpokemonIndex(pokemonIndex + 1)
+  }
+
+    // *** Insertion tableau pokemonList
+    const pokemonList = [
+      {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "charmander",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        },
+        {
+          name: "squirtle",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+        },
+        {
+          name: "pikachu",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+        },
+        {
+          name: "mew",
+        },
+      ];  
+
+      return (
+        <div>
+        
+          <PokemonCard name={pokemonList[pokemonIndex].name} imgSrc={pokemonList[pokemonIndex].imgSrc}/>
+          {pokemonIndex > 0 ? <button onClick={handleClickp}>précédent</button> : ""}
+          {pokemonIndex < pokemonList.length - 1 ?<button onClick={handleClicks}>suivant</button> : ""}
+          
+        </div>
+      );
+    
 }
 
 export default App;
